e92a552444693187f065e01cbcf3281ee4c0ecb5
sws <sws@paywith.co.kr>
2021-04-11 16:09:32 +0900

    현재 스크롤높이에 따른 secton scene 값 변경되도록 로직 작성

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -9,7 +9,10 @@ export class MainComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    const seceInfo = [
+    let yOffset = 0; //window.pageYOffset 대신
+    let prevScrollHeight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0; //현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+    const sceneInfo = [
       {
         typeof: 'sticky',
         heightNum: 5, //브라우저 높이의 5배로 scrollHeight 세팅
@@ -48,13 +51,35 @@ export class MainComponent implements OnInit {
 
     /** 각 스크롤의 섹션 높이 세팅 */
     function setLayout() {
-      for (let i = 0; i < seceInfo.length; i++) {
-        seceInfo[i].scrollHeight = seceInfo[i].heightNum * window.innerHeight;
-        (seceInfo[i].objs
-          .container as HTMLElement).style.height = `${seceInfo[i].scrollHeight}px`;
+      for (let i = 0; i < sceneInfo.length; i++) {
+        sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+        (sceneInfo[i].objs
+          .container as HTMLElement).style.height = `${sceneInfo[i].scrollHeight}px`;
       }
     }
 
+    /** */
+    function scrollLoop() {
+      prevScrollHeight = 0;
+      for (let i = 0; i < currentScene; i++) {
+        prevScrollHeight += sceneInfo[i].scrollHeight;
+      }
+
+      if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+        currentScene++;
+      }
+
+      if (yOffset < prevScrollHeight) {
+        currentScene--;
+      }
+
+      console.log(currentScene);
+    }
+
     window.addEventListener('resize', setLayout);
+    window.addEventListener('scroll', () => {
+      yOffset = window.pageYOffset;
+      scrollLoop();
+    });
   }
 }
