5e2e56f1df9ac0ee68f0a4c9b5792418deda87ca
sws <sws@paywith.co.kr>
2021-04-11 17:56:31 +0900

    특정 스크롤 위치에서도 새로고침 시 sticky elem 문구가 보일 수 있도록 로직 작성

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -12,6 +12,11 @@ export class MainComponent implements OnInit {
     let yOffset = 0; //window.pageYOffset 대신
     let prevScrollHeight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
     let currentScene = 0; //현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+    let bodyElem = document.querySelector('.content-body');
+    //기본 섹션값 및 새로고침시 sticky elem이 보일 수 있도록 대응
+
+    bodyElem.setAttribute('id', `show-scene-0`);
+
     const sceneInfo = [
       {
         typeof: 'sticky',
@@ -56,6 +61,20 @@ export class MainComponent implements OnInit {
         (sceneInfo[i].objs
           .container as HTMLElement).style.height = `${sceneInfo[i].scrollHeight}px`;
       }
+
+      yOffset = window.pageYOffset;
+
+      //해당 스크롤 섹션 및 스크롤위치에서 새로고침시 sticky elem 정보가 보이도록 작업
+      //각 섹션마다 높이 누적값과 현재 스크롤 위치값을 비교
+      let totalScrollHeight = 0;
+      for (let i = 0; i < sceneInfo.length; i++) {
+        totalScrollHeight += sceneInfo[i].scrollHeight;
+
+        if (totalScrollHeight >= yOffset) {
+          currentScene = i;
+          break;
+        }
+      }
     }
 
     /** */
@@ -70,12 +89,17 @@ export class MainComponent implements OnInit {
       }
 
       if (yOffset < prevScrollHeight) {
+        if (currentScene === 0) return; //브라우저 바운스 효과로 인해(e. 아이폰 새로고침) 마이너스가 되는 것을 방지(모바일)
         currentScene--;
       }
 
-      console.log(currentScene);
+      //angular에서 해당 컴포넌트 내 외부 tag를 참조할 수 없습니다:)
+      // document.body.setAttribute('id', `show-scene-${currentScene}`);
+      bodyElem.setAttribute('id', `show-scene-${currentScene}`);
     }
 
+    //window.addEventListener('DOMContentLoaded', setLayout);
+    window.addEventListener('load', setLayout);
     window.addEventListener('resize', setLayout);
     window.addEventListener('scroll', () => {
       yOffset = window.pageYOffset;
