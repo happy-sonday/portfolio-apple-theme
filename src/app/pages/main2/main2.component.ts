192c9adf19ab83bfa3ee23baf3896981d7a9d92c
sws <sws@paywith.co.kr>
2021-04-23 08:38:42 +0900

    단일 문장 fade-in&out 조절 함수 생성

--- src/app/pages/main2/main2.component.ts
+++ src/app/pages/main2/main2.component.ts
@@ -22,6 +22,13 @@ export class Main2Component implements OnInit {
         scrollHeight: 0,
         objs: {
           container: document.querySelector('#scroll-section-0'),
+          messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+          messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+          messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+          messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+        },
+        values: {
+          messageA_opacity: [0, 1],
         },
       },
       {
@@ -73,6 +80,36 @@ export class Main2Component implements OnInit {
         .setAttribute('id', `show-scene-section-${currentScene}`);
     }
 
+    function calcValues(values, currentYOffset) {
+      let rv;
+      let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+      rv = scrollRatio * (values[1] - values[0]) + values[0];
+      return rv;
+    }
+    function playAnimation() {
+      const objs = sceneInfo[currentScene].objs;
+      const values = sceneInfo[currentScene].values;
+      const currentYOffset = yOffset - prevScrollHeight;
+
+      switch (currentScene) {
+        case 0:
+          let messageA_opacity_in = calcValues(
+            values.messageA_opacity,
+            currentYOffset
+          );
+          console.log(messageA_opacity_in);
+          (objs.messageA as HTMLElement).style.opacity = messageA_opacity_in;
+
+          break;
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+      }
+    }
+
     function scrollLoop() {
       prevScrollHeight = 0;
       for (let i = 0; i < currentScene; i++) {
@@ -81,18 +118,21 @@ export class Main2Component implements OnInit {
 
       if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
         currentScene++;
+        document
+          .querySelector('.content-body')
+          .setAttribute('id', `show-scene-section-${currentScene}`);
       }
 
       if (yOffset < prevScrollHeight) {
         if (currentScene === 0) return;
         currentScene--;
+        document
+          .querySelector('.content-body')
+          .setAttribute('id', `show-scene-section-${currentScene}`);
       }
-      console.log(currentScene);
 
-      document
-        .querySelector('.content-body')
-        .setAttribute('id', `show-scene-section-${currentScene}`);
       //document.body.setAttribute('id', `show-scene-section-${currentScene}`);
+      playAnimation();
     }
 
     window.addEventListener('scroll', () => {
