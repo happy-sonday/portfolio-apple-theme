54220a05597584098be06c6cce9b597fbe6d5006
sws <sws@paywith.co.kr>
2021-04-23 09:46:12 +0900

    section1에서 단일문장만 fade, translate 적용 완료

--- src/app/pages/main2/main2.component.ts
+++ src/app/pages/main2/main2.component.ts
@@ -29,7 +29,10 @@ export class Main2Component implements OnInit {
           messageD: document.querySelector('#scroll-section-0 .main-message.d'),
         },
         values: {
-          messageA_opacity: [0, 1],
+          messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+          messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+          messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+          messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }],
         },
       },
       {
@@ -83,23 +86,73 @@ export class Main2Component implements OnInit {
 
     function calcValues(values, currentYOffset) {
       let rv;
-      let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
-      rv = scrollRatio * (values[1] - values[0]) + values[0];
+      let scrollHeight = sceneInfo[currentScene].scrollHeight;
+      let scrollRatio = currentYOffset / scrollHeight;
+
+      //객체 프로퍼티 갯수가 3개인 경우
+      if (values.length === 3) {
+        //start~end 사이에 애니메이션 실행
+        const partScrollStart = values[2].start * scrollHeight;
+        const partScrollEnd = values[2].end * scrollHeight;
+        const partScrollHeight = partScrollEnd - partScrollStart;
+        //rv = scrollRatio * (values[1] - values[0]) + values[0];
+
+        if (
+          currentYOffset >= partScrollStart &&
+          currentYOffset <= partScrollEnd
+        ) {
+          rv =
+            ((currentYOffset - partScrollStart) / partScrollHeight) *
+              (values[1] - values[0]) +
+            values[0];
+        } else if (currentYOffset < partScrollStart) {
+          rv = values[0];
+        } else if (currentYOffset > partScrollEnd) {
+          rv = values[1];
+        }
+      } else {
+        rv = scrollRatio * (values[1] - values[0]) + values[0];
+      }
+
       return rv;
     }
     function playAnimation() {
       const objs = sceneInfo[currentScene].objs;
       const values = sceneInfo[currentScene].values;
       const currentYOffset = yOffset - prevScrollHeight;
+      const scrollHeight = sceneInfo[currentScene].scrollHeight;
+      //const scrollRatio = (yOffset-prevScrollHeight) / scrollHeight; //현재 씬의 scrollHeight
+      const scrollRatio = currentYOffset / scrollHeight; //현재 씬의 scrollHeight
 
       switch (currentScene) {
         case 0:
-          let messageA_opacity_in = calcValues(
-            values.messageA_opacity,
+          const messageA_opacity_in = calcValues(
+            values.messageA_opacity_in,
             currentYOffset
           );
+          const messageA_opacity_out = calcValues(
+            values.messageA_opacity_out,
+            currentYOffset
+          );
+          const messageA_translateY_in = calcValues(
+            values.messageA_translateY_in,
+            currentYOffset
+          );
+          const messageA_translateY_out = calcValues(
+            values.messageA_translateY_out,
+            currentYOffset
+          );
+
+          if (scrollRatio <= 0.22) {
+            //in
+            (objs.messageA as HTMLElement).style.opacity = messageA_opacity_in;
+            (objs.messageA as HTMLElement).style.transform = `translateY(${messageA_translateY_in}%)`;
+          } else {
+            //out
+            (objs.messageA as HTMLElement).style.opacity = messageA_opacity_out;
+            (objs.messageA as HTMLElement).style.transform = `translateY(${messageA_translateY_out}%)`;
+          }
           console.log(messageA_opacity_in);
-          (objs.messageA as HTMLElement).style.opacity = messageA_opacity_in;
 
           break;
         case 1:
