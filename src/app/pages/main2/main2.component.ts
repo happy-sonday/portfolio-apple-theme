39980a7e2473c43d8ba4f4d4fe7747ba006af814
sws <sws@paywith.co.kr>
2021-04-24 16:03:25 +0900

    video section2 완성

--- src/app/pages/main2/main2.component.ts
+++ src/app/pages/main2/main2.component.ts
@@ -76,6 +76,11 @@ export class Main2Component implements OnInit {
           messageC: document.querySelector('#scroll-section-2 .c'),
           pinB: document.querySelector('#scroll-section-2 .b .pin'),
           pinC: document.querySelector('#scroll-section-2 .c .pin'),
+          canvas: document.querySelector('#video-canvas-1'),
+          context: (document.querySelector(
+            '#video-canvas-1'
+          ) as HTMLCanvasElement).getContext('2d'),
+          videoImages: [],
         },
         values: {
           messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
@@ -92,6 +97,10 @@ export class Main2Component implements OnInit {
           messageC_opacity_out: [1, 0, { start: 0.95, end: 1 }],
           pinB_scaleY: [0.5, 1, { start: 0.6, end: 0.65 }],
           pinC_scaleY: [0.5, 1, { start: 0.87, end: 0.92 }],
+          videoImageCount: 960,
+          imageSequence: [0, 959],
+          canvas_opacity_in: [0, 1, { start: 0, end: 0.1 }],
+          canvas_opacity_out: [1, 0, { start: 0.95, end: 1 }],
         },
       },
       {
@@ -113,6 +122,12 @@ export class Main2Component implements OnInit {
         imgElem = new Image();
         imgElem.src = `/assets/video/001/IMG_${6726 + i}.JPG`;
         sceneInfo[0].objs.videoImages.push(imgElem);
+      }
+      let imgElem2;
+      for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
+        imgElem2 = new Image();
+        imgElem2.src = `/assets/video/002/IMG_${7027 + i}.JPG`;
+        sceneInfo[2].objs.videoImages.push(imgElem2);
       }
     }
 
@@ -145,6 +160,8 @@ export class Main2Component implements OnInit {
 
       const heightRatio = window.innerHeight / 1080;
       (sceneInfo[0].objs
+        .canvas as HTMLCanvasElement).style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
+      (sceneInfo[2].objs
         .canvas as HTMLCanvasElement).style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
     }
 
@@ -289,6 +306,22 @@ export class Main2Component implements OnInit {
           break;
 
         case 2:
+          let sequence2 = Math.round(
+            calcValues(values.imageSequence, currentYOffset)
+          );
+          objs.context.drawImage(objs.videoImages[sequence2], 0, 0);
+
+          if (scrollRatio <= 0.5) {
+            (objs.canvas as HTMLCanvasElement).style.opacity = calcValues(
+              values.canvas_opacity_in,
+              currentYOffset
+            );
+          } else {
+            (objs.canvas as HTMLCanvasElement).style.opacity = calcValues(
+              values.canvas_opacity_out,
+              currentYOffset
+            );
+          }
           if (scrollRatio <= 0.32) {
             // in
             (objs.messageA as HTMLElement).style.opacity = calcValues(
